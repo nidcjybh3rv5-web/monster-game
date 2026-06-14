016276279cda9f7fe92a2ad2d5574dc3c53e7cea
Apple_14 <nidcjybh3rv5@gmail.com>
2026-06-14 16:21:28 +0800

    Delete js directory

--- js/effects.js
+++
@@ -1,2 +0,0 @@
-function flashScreen() { document.body.classList.add('flash'); setTimeout(() => document.body.classList.remove('flash'), 200); }
-function shakeScreen() { document.querySelector('.app').classList.add('shake'); setTimeout(() => document.querySelector('.app').classList.remove('shake'), 200); }
